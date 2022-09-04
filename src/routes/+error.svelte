c68bc76a201a4bd32cc22c312c88f815c079a21c
mac172 <king7mac@gmail.com>
2022-09-04 10:17:36 +0530

    change styles

---
+++ src/routes/+error.svelte
@@ -0,0 +1,12 @@
+<script>
+  /// This file handle all error occured
+  import { page } from '$app/stores'
+</script>
+
+<h2>{$page.status}</h2>
+<p class="subhead">{$page.error.message}</p>
+
+<p><strong>Sorry!</strong> Maybe try one of these links?</p>
+<ul>
+	<li><a href="/">Home</a></li>
+</ul>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,6 +9,10 @@
   <title>Home</title>
 </svelte:head>
 
+
+<div class="ball1"></div>
+<span class="ball2"></span>
+
 <div>
   <ul>
     {#each category as cat}
@@ -24,6 +28,7 @@
 <style>
   ul {
     display: block;
+    margin-right: 20px;
 
   }
 
@@ -37,9 +42,12 @@
    /* border-radius: 14px;
     background: #0a143c;*/
     border-radius: 21px;
-    background: rgba(0,0,0,0.74);
-    box-shadow:  9px 9px 0px #05091c,
-                 -9px -9px 0px #0f1f5c;
+    background: rgba(156, 128, 185, 0.11);
+    border-radius: 16px;
+    box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+    backdrop-filter: blur(2.4px);
+    -webkit-backdrop-filter: blur(2.4px);
+    border: 1px solid rgba(156, 128, 185, 0.89);
   }
 
   li,a {
@@ -58,9 +66,74 @@
   }
 
   p {
-    display: flex;
-    justify-content: center;
-    align-items: center;
+    display: inline-flex;
+    text-align: center;
     font-family: monospace;
+    overflow: hidden;
+    width: 0%;
+    color: #FFFCF9;
+    margin-left: 20px;
+    white-space: nowrap;
+    animation: typewriter 4s linear infinite alternate;
+  }
+
+  @keyframes typewriter {
+
+    90%,100%{width:100%;}
+  }
+
+  .ball1 {
+    position: absolute;
+    background: #FC6076;
+    width: 200px;
+    height: 300px;
+    color: #44436A;
+    top: 50%;
+    left: -30%; 
+    border-radius: 30% 70% 70% 30% / 31% 30% 70% 69%;
+  }
+
+  .ball2 {
+    position: absolute;
+    width: 223px;
+    height: 150px;
+    top: -8%;
+    z-index: -1;
+    left: 25%;
+    background: #F4D35E;
+    border-radius: 45% 14% 38% 31% / 19% 42% 41% 38% ;
+  }
+
+
+  @media only screen and (min-width: 768px) {
+    .ball2 {
+      position: absolute;
+      width: 300px;
+      height: 230px;
+      left: 37%;
+    }
+
+    .ball1 {
+      position: absolute;
+      width: 580px;
+      height: 570px;
+      left: -18%;
+      top: 30%;
+      animation: fader 4s linear infinite alternate;
+    }
+
+    ul {
+      margin-right: 15%;
+    }
+
+    p {
+      margin-left: 33%;
+      padding-right: -17%;
+    }
+
+    @keyframes fader {
+      0% {opacity: 10%;}
+      100% {opacity: 100%}
+    }
   }
 </style>
