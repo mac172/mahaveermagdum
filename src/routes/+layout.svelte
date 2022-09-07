405f0040a1c48445ad36f0f1291edfeeee5bb2f2
mac172 <king7mac@gmail.com>
2022-09-07 20:23:43 +0530

    add vitals.js to layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,18 @@
 <script>
+  import { webVitals } from '$lib/vitals';
+  import { browser } from '$app/env';
+  import { page } from '$app/stores';
+
+  let analyticsId = import.meta.env.VERCEL_ANALYTICS_ID;
+
+  $: if (browser && analyticsId) {
+    webVitals({
+      path: $page.url.pathname,
+      params: $page.params,
+      analyticsId
+    })
+  }
+
   // Notice the import path. $lib is a handy alias that goes directly to src/lib, saving you from typing tedious relative paths. (You can customize this alias or add your own in jsconfig.jso)
   import Header from '$lib/components/Header.svelte'
   import '$lib/styles/style.css'
@@ -16,4 +30,6 @@
 <main in:fade={{duration: 300, delay: 150}} out:fade={{duration:300}}>
   <slot />
 </main>
+
 {/key}
+
